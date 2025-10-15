ae0759177d8187037b5fedab47255462275a7f52
doruk.tekel <doruktekel55@gmail.com>
2025-10-15 17:08:30 +0300

    added video to video page

--- src/app/video/page.jsx
+++ src/app/video/page.jsx
@@ -1,3 +1,48 @@
+// import BackButton from "@/components/BackButton";
+// import Image from "next/image";
+// import Link from "next/link";
+// import React from "react";
+
+// const VideoPage = () => {
+//   return (
+//     <div className="w-full min-h-screen overflow-hidden relative flex items-center justify-center pb-4 md:pb-10">
+//       <BackButton />
+//       <div
+//         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+//         style={{
+//           backgroundImage: "url('/kapak.webp')",
+//           filter: "brightness(1.1) contrast(1.1) saturate(1.1)",
+//         }}
+//       ></div>
+
+//       <Link
+//         href="https://belenandpartners.com/"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//         className="absolute top-4 md:top-8 md:left-auto right-8 bg-white/20 backdrop-blur-md px-3 py-2 md:px-5 md:py-3 rounded-lg md:rounded-2xl flex items-center justify-center gap-2 text-white cursor-pointer hover:text-gray-400 transition-all duration-300 md:text-xl"
+//       >
+//         <Image
+//           src="/logo.png"
+//           alt="Villa Belen Logo"
+//           width={160}
+//           height={160}
+//           className="w-36 md:w-44"
+//         />
+//       </Link>
+
+//       <video
+//         src="/outputvillabelen.mp4"
+//         autoPlay
+//         loop
+//         muted
+//         className="w-full h-full object-cover"
+//       ></video>
+//     </div>
+//   );
+// };
+
+// export default VideoPage;
+
 import BackButton from "@/components/BackButton";
 import Image from "next/image";
 import Link from "next/link";
@@ -19,7 +64,7 @@ const VideoPage = () => {
         href="https://belenandpartners.com/"
         target="_blank"
         rel="noopener noreferrer"
-        className="absolute top-4 md:top-8 md:left-auto right-8 bg-white/20 backdrop-blur-md px-3 py-2 md:px-5 md:py-3 rounded-lg md:rounded-2xl flex items-center justify-center gap-2 text-white cursor-pointer hover:text-gray-400 transition-all duration-300 md:text-xl"
+        className="absolute top-4 md:top-8 md:left-auto right-8 bg-white/20 backdrop-blur-md px-3 py-2 md:px-5 md:py-3 rounded-lg md:rounded-2xl flex items-center justify-center gap-2 text-white cursor-pointer hover:text-gray-400 transition-all duration-300 md:text-xl z-10"
       >
         <Image
           src="/logo.png"
@@ -29,6 +74,14 @@ const VideoPage = () => {
           className="w-36 md:w-44"
         />
       </Link>
+
+      <video
+        src="/outputvillabelen.mp4"
+        loop
+        controls
+        // className="md:max-w-7xl max-w-2xl h-full object-cover z-50 rounded-2xl"
+        className=" md:max-w-7xl px-4 md:px-0 h-auto z-50 rounded-2xl"
+      ></video>
     </div>
   );
 };
